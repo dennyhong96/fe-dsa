98c6eda7a13de421eb708049cc1152ea4baf0d31
Denny Hong <hong961127@gmail.com>
2021-11-21 20:01:53 -0800

    Re-implement bubbleSort

--- src/13.Sorting/bubbleSort.ts
+++ src/13.Sorting/bubbleSort.ts
@@ -1,12 +1,18 @@
 // O(n^2)
 export function bubbleSort(arr: number[]): void {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      const currItem = arr[j];
-      const compItem = arr[j + 1];
-      if (currItem > compItem) {
-        arr[j + 1] = currItem;
-        arr[j] = compItem;
+  let hasSwapped = true;
+
+  // Stop the while loop when there is no swaps in the previous loop,
+  // which means the list is fully sorted
+  while (hasSwapped === true) {
+    hasSwapped = false;
+    for (let j = 1; j < arr.length; j++) {
+      const currItem = arr[j - 1];
+      const nextItem = arr[j];
+      if (currItem > nextItem) {
+        arr[j] = currItem;
+        arr[j - 1] = nextItem;
+        hasSwapped = true;
       }
     }
   }
