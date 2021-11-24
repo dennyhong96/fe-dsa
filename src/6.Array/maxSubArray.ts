ac60fa974c8f47c45bc6ab3d58c6eb95f7c6ff20
Denny Hong <hong961127@gmail.com>
2021-11-24 12:50:58 -0800

    Implement maxSubArray

--- src/6.Array/maxSubArray.ts
+++ src/6.Array/maxSubArray.ts
@@ -1,27 +1,16 @@
-// Given an integer array nums, find the contiguous subarray
-// (containing at least one number) which has the largest sum and return its sum.
-// A subarray is a contiguous part of an array.
-// Example:
-// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-// Output: 6
-// Explanation: [4,-1,2,1] has the largest sum = 6.
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+export function maxSubArray(nums: number[]) {
+  let maxSum = -Infinity;
+  let maxContiguousSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const newContiguousSum = maxContiguousSum + num;
 
-export function maxSubArray(nums:number[]) {
-  if (!nums.length) {
-    throw new Error('Array cannot be empty.')
-  }
-  let maxContiguousSum: number = nums[0];
-  let maxSum: number = maxContiguousSum;
-  for (let i = 1; i < nums.length; i++) {
-      const num = nums[i];
-
-      // Check if which one is larger
-      // Adding up this number with previous sum, or this number itself
-      const newMaxContiguousSum = num + maxContiguousSum;
-      maxContiguousSum = Math.max(newMaxContiguousSum, num);
-      if (maxContiguousSum > maxSum) {
-          maxSum = maxContiguousSum;
-      }
+    // Check which one is larger -
+    // Adding up this number with previous maxContiguousSum, or this number by itself
+    maxContiguousSum = Math.max(newContiguousSum, num);
+    maxSum = Math.max(maxSum, maxContiguousSum);
   }
   return maxSum;
 }
