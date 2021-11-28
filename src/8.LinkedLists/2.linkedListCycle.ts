af0cb854a7b14eb4fb0918e1c16d9fd5a8186ef9
Denny Hong <hong961127@gmail.com>
2021-11-28 10:26:46 -0800

    Implement linkedListCycle

---
+++ src/8.LinkedLists/2.linkedListCycle.ts
@@ -0,0 +1,32 @@
+import { ListNode } from "./LinkedList";
+
+// floyd tortoise and hare algorithm (Fast & slow pointers)
+// O(n) time; O(1) space
+function hasCycle(head: ListNode | null): boolean {
+  if (!head) return false;
+  let fastPointerNode: ListNode | null = head;
+  let slowPointerNode = head;
+  while (fastPointerNode && fastPointerNode.next) {
+    fastPointerNode = fastPointerNode.next.next;
+    slowPointerNode = slowPointerNode.next!;
+    if (slowPointerNode === fastPointerNode) return true;
+  }
+  return false;
+}
+
+// O(n) time; O(n) space
+function hasCycle1(head: ListNode | null): boolean {
+  if (!head) return false;
+  let currNode = head;
+  const map = new Map<ListNode, boolean>();
+  while (currNode.next) {
+    const storedNodeIndex = map.get(currNode);
+    if (storedNodeIndex) {
+      return true;
+    } else {
+      map.set(currNode, true);
+      currNode = currNode.next;
+    }
+  }
+  return false;
+}
