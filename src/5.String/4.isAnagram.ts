589fefb3fa1467ae63cffc1e41bc417981042121
Denny Hong <hong961127@gmail.com>
2021-12-02 15:16:00 -0800

    Implement isAnagram

---
+++ src/5.String/4.isAnagram.ts
@@ -0,0 +1,27 @@
+/**
+ * 242. Valid Anagram
+ * https://leetcode.com/problems/valid-anagram/
+ */
+
+// O(n) time; O(1) space
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  const map = new Map<string, number>(); // O(1) space since at most size of 26
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    map.set(sChar, (map.get(sChar) ?? 0) + 1);
+  }
+
+  let isAna = true;
+  for (let i = 0; i < t.length; i++) {
+    const tChar = t[i];
+    if (!map.get(tChar)) {
+      isAna = false;
+      break;
+    }
+    map.set(tChar, (map.get(tChar) ?? 0) - 1);
+  }
+
+  return isAna;
+}
