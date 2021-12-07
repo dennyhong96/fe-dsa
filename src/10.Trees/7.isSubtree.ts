fc1141e935a0bffb5a3a2fb805190cdd8017054d
Denny Hong <hong961127@gmail.com>
2021-12-07 02:12:51 -0800

    Re-implement isSubtree

--- src/10.Trees/7.isSubtree.ts
+++ src/10.Trees/7.isSubtree.ts
@@ -4,30 +4,20 @@ import { TreeNode } from "./BinarySearchTree";
  * 572. Subtree of Another Tree
  * https://leetcode.com/problems/subtree-of-another-tree/
  */
-// O(m * n) time, O(k) space - k is the max depth larger tree
+// Two DFS - O(m * n) time, O(k) space - k is the max depth of root
 function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
   if (!subRoot) return true;
   if (!root) return false;
-  if (isSameTreeAsRoot(root, subRoot)) return true;
-  const isLeftSubtreeOfRoot = isSubtree(root.left, subRoot);
-  const isRightSubtreeOfRoot = isSubtree(root.right, subRoot);
-  return isLeftSubtreeOfRoot || isRightSubtreeOfRoot;
+  if (isSameTree(root, subRoot)) return true;
+  const isLeftSubtree = isSubtree(root.left, subRoot);
+  const isRightSubtree = isSubtree(root.right, subRoot);
+  return isLeftSubtree || isRightSubtree;
 }
 
-// O(n) time - n is the size the smaller tree
-function isSameTreeAsRoot(
-  root1: TreeNode | null,
-  root2: TreeNode | null
-): boolean {
-  if (
-    (!root1 && root2) ||
-    (root1 && !root2) ||
-    (root1 && root2 && root1.val !== root2.val)
-  ) {
-    return false;
-  }
-  if (!root1 && !root2) return true;
-  const isLeftSameTree = isSameTreeAsRoot(root1!.left, root2!.left);
-  const isRightSameTree = isSameTreeAsRoot(root1!.right, root2!.right);
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if ((p && !q) || (!p && q) || (p && q && p.val !== q.val)) return false;
+  if (!p && !q) return true;
+  const isLeftSameTree = isSameTree(p!.left, q!.left);
+  const isRightSameTree = isSameTree(p!.right, q!.right);
   return isLeftSameTree && isRightSameTree;
 }
