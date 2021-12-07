1f9693a70db7d735320c9a3268938e61afba88f0
Denny Hong <hong961127@gmail.com>
2021-12-07 01:06:51 -0800

    Re-implement isSameTree

--- src/10.Trees/2.isSameTree.ts
+++ src/10.Trees/2.isSameTree.ts
@@ -4,15 +4,12 @@ import { TreeNode } from "./BinarySearchTree";
  * 100. Same Tree
  * https://leetcode.com/problems/same-tree/
  */
-// O(n) time - n is the min node between p and q tree; O(m) space - m is the min depth between p and q tree
+
+// O(n) time; O(m) space - m is depth of tree
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  if ((!p && q) || (p && !q) || (p && q && p.val !== q.val)) {
-    return false;
-  } else if (!p && !q) {
-    return true;
-  } else {
-    const isLeftSubtreeSame = isSameTree(p!.left, q!.left);
-    const isRightSubtreeSame = isSameTree(p!.right, q!.right);
-    return isLeftSubtreeSame && isRightSubtreeSame;
-  }
+  if ((p && !q) || (!p && q) || (p && q && p.val !== q.val)) return false;
+  if (!p && !q) return true;
+  const isLeftSameTree = isSameTree(p!.left, q!.left);
+  const isRightSameTree = isSameTree(p!.right, q!.right);
+  return isLeftSameTree && isRightSameTree;
 }
