6383a15f69a38417eaa97af9f6d3f43e2757beff
Denny Hong <hong961127@gmail.com>
2021-12-10 02:06:55 -0800

    implement levelOrder

--- src/10.Trees/5.levelOrder.ts
+++ src/10.Trees/5.levelOrder.ts
@@ -8,44 +8,44 @@ import { TreeNode } from "./BinarySearchTree";
 // Iterative - O(n) time; O(n/2) space (https://youtu.be/6ZnyEApgFYg?t=352)
 function levelOrder(root: TreeNode | null): number[][] {
   if (!root) return [];
-  const queue: TreeNode[] = [];
   const result: number[][] = [];
+  const queue: TreeNode[] = [];
   queue.push(root);
   while (queue.length) {
-    const levelNodesCount = queue.length;
-    const levelResult: number[] = [];
-    for (let i = 0; i < levelNodesCount; i++) {
+    const levelNodesLength = queue.length;
+    let levelResult: number[] = [];
+    for (let i = 0; i < levelNodesLength; i++) {
       const node = queue.shift()!;
       levelResult.push(node.val);
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
-    if (levelResult.length) result.push(levelResult);
+    result.push(levelResult);
   }
   return result;
 }
 
 // Recursive - O(n) time; O(n/2) space (https://youtu.be/6ZnyEApgFYg?t=352)
-function levelOrder1(root: TreeNode | null): number[][] {
-  if (!root) return [];
-  const queue: { node: TreeNode; level: number }[] = [];
-  const result: number[][] = [];
-  queue.push({ node: root, level: 1 });
-  let currLevel = 0;
-  const traverse = () => {
-    if (queue.length) {
-      const { node, level } = queue.shift()!;
-      if (level > currLevel) {
-        result.push([node.val]);
-        currLevel = level;
-      } else {
-        result[result.length - 1].push(node.val);
-      }
-      if (node.left) queue.push({ node: node.left, level: level + 1 });
-      if (node.right) queue.push({ node: node.right, level: level + 1 });
-      traverse();
-    }
-  };
-  traverse();
+function levelOrder2(
+  root: TreeNode | null,
+  maxLevel = 1,
+  queue: { node: TreeNode; level: number }[] = root
+    ? [{ node: root, level: 1 }]
+    : [],
+  result: number[][] = []
+): number[][] {
+  if (!queue.length) return [];
+  const { node, level } = queue.shift()!;
+  if (level > maxLevel) {
+    result.push([node.val]);
+  } else {
+    if (!result[result.length - 1]) result.push([]);
+    result[result.length - 1].push(node.val);
+  }
+  if (node.left) queue.push({ node: node.left, level: level + 1 });
+  if (node.right) queue.push({ node: node.right, level: level + 1 });
+  levelOrder2(null, level, queue, result);
   return result;
 }
+
+export {};
