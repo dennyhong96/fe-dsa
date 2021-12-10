4ad9e45a3e4f408554b4a596a9aabd3dd2ecfa37
Denny Hong <hong961127@gmail.com>
2021-12-10 02:22:31 -0800

    Implement isSubtree

--- src/10.Trees/7.isSubtree.ts
+++ src/10.Trees/7.isSubtree.ts
@@ -4,20 +4,24 @@ import { TreeNode } from "./BinarySearchTree";
  * 572. Subtree of Another Tree
  * https://leetcode.com/problems/subtree-of-another-tree/
  */
-// Two DFS - O(m * n) time, O(k) space - k is the max depth of root
+
+// O(n * m) time; O(h * i) space -h is height of root, i is height of subRoot;
 function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
-  if (!subRoot) return true;
   if (!root) return false;
+  if (!subRoot) return true;
   if (isSameTree(root, subRoot)) return true;
   const isLeftSubtree = isSubtree(root.left, subRoot);
   const isRightSubtree = isSubtree(root.right, subRoot);
   return isLeftSubtree || isRightSubtree;
 }
 
-function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  if ((p && !q) || (!p && q) || (p && q && p.val !== q.val)) return false;
-  if (!p && !q) return true;
-  const isLeftSameTree = isSameTree(p!.left, q!.left);
-  const isRightSameTree = isSameTree(p!.right, q!.right);
-  return isLeftSameTree && isRightSameTree;
+// O(n) time; O(h) space - h is height of smaller tree
+function isSameTree(a: TreeNode | null, b: TreeNode | null): boolean {
+  if ((!a && b) || (a && !b) || (a && b && a.val !== b.val)) return false;
+  if (!a && !b) return true;
+  const isLeftSubtreeSame = isSameTree(a!.left, b!.left);
+  const isRightSubtreeSame = isSameTree(a!.right, b!.right);
+  return isLeftSubtreeSame && isRightSubtreeSame;
 }
+
+export {};
