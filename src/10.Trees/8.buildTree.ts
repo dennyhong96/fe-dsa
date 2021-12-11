71a6c1852310a95f3288d20cc572e363e646024a
Denny Hong <hong961127@gmail.com>
2021-12-10 16:07:06 -0800

    implement buildTree

--- src/10.Trees/8.buildTree.ts
+++ src/10.Trees/8.buildTree.ts
@@ -9,11 +9,11 @@ import { TreeNode } from "./BinarySearchTree";
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
   if (!inorder.length) return null;
   const num = preorder.shift()!;
-  const node = new TreeNode(num);
-  const mid = inorder.findIndex((n) => n === num);
-  const leftInOrder = inorder.slice(0, mid);
-  const rightInOrder = inorder.slice(mid + 1);
-  node.left = buildTree(preorder, leftInOrder);
-  node.right = buildTree(preorder, rightInOrder);
-  return node;
+  const inorderNodeIndex = inorder.findIndex((n) => n === num);
+  const root = new TreeNode(num);
+  root.left = buildTree(preorder, inorder.slice(0, inorderNodeIndex));
+  root.right = buildTree(preorder, inorder.slice(inorderNodeIndex + 1));
+  return root;
 }
+
+export {};
