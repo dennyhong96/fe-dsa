83ea12ed2c29d506920accc47689f87b42d664bd
Denny Hong <hong961127@gmail.com>
2021-12-10 20:02:41 -0800

    Implement kthSmallest

--- src/10.Trees/10.kthSmallest.ts
+++ src/10.Trees/10.kthSmallest.ts
@@ -5,34 +5,56 @@ import { TreeNode } from "./BinarySearchTree";
  * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
  */
 
-// Recursive pre-order - O(n) time; O(n) space
+// Recursive preorder DFS, O(n) time; O(n) space
 function kthSmallest(root: TreeNode | null, k: number): number {
-  if (!root) return -1;
-  const inorder: number[] = [];
-  const traverse = (node: TreeNode) => {
-    if (node.left) traverse(node.left);
-    inorder.push(node.val);
-    if (node.right) traverse(node.right);
-    return node;
+  const result: number[] = [];
+  const dfs = (root: TreeNode | null) => {
+    if (!root) return;
+    if (root.left) dfs(root.left);
+    result.push(root.val);
+    if (root.right) dfs(root.right);
   };
-  traverse(root);
-  return inorder[k - 1];
+  dfs(root);
+  return result[k - 1];
 }
 
-// Iterative pre-order - O(n) time; O(n) space;
+// Iterative preorder DFS - O(n) time; O(n) space
 function kthSmallest1(root: TreeNode | null, k: number): number {
   let n = 0;
   const stack: TreeNode[] = [];
-  let curr = root;
-  while (curr || stack.length) {
-    while (curr) {
-      stack.push(curr);
-      curr = curr.left;
+  let currNode = root;
+  while (currNode || stack.length) {
+    while (currNode) {
+      stack.push(currNode);
+      currNode = currNode.left;
     }
     const node = stack.pop()!;
     n++;
     if (n === k) return node.val;
-    curr = node.right;
+    currNode = node.right;
   }
   return -1;
 }
+
+// Iterative preorder DFS - O(n) time; O(n) space
+function kthSmallest2(root: TreeNode | null, k: number): number {
+  const stack: TreeNode[] = [];
+  const result: number[] = [];
+  let currNode = root;
+  while (currNode) {
+    stack.push(currNode);
+    currNode = currNode.left;
+  }
+  while (stack.length) {
+    const node = stack.pop()!;
+    result.push(node.val);
+    let curr = node.right;
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+  }
+  return result[k - 1];
+}
+
+export {};
