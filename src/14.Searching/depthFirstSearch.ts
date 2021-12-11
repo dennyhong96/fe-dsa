1273703117ac0cd9efac7e4aefc3be62b190205f
Denny Hong <hong961127@gmail.com>
2021-12-10 20:42:47 -0800

    Add iterative preorder dfs

--- src/14.Searching/depthFirstSearch.ts
+++ src/14.Searching/depthFirstSearch.ts
@@ -25,6 +25,18 @@ export function dfsPreOrder(root: TreeNode, result: number[] = []) {
   return result;
 }
 
+export function dfsPreOrder2(root: TreeNode, result: number[] = []) {
+  const stack: TreeNode[] = []; // Use a stack to simulate call stack in recursive solution
+  stack.push(root);
+  while (stack.length) {
+    const node = stack.pop()!;
+    result.push(node.val);
+    if (node.right) stack.push(node.right); // Push right child to stack first
+    if (node.left) stack.push(node.left); // Push left child to stack last, so it gets poped out first
+  }
+  return result;
+}
+
 export function dfsPostOrder(root: TreeNode, result: number[] = []) {
   if (root.left) dfsPostOrder(root.left);
   if (root.right) dfsPostOrder(root.right);
