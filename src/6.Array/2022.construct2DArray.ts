5b5d2dc6dccb704aee683f778ab644c064d3e409
Denny Hong <hong961127@gmail.com>
2022-02-03 00:40:44 -0800

    Implement 2022.construct2DArray

---
+++ src/6.Array/2022.construct2DArray.ts
@@ -0,0 +1,19 @@
+function construct2DArray(
+  original: number[],
+  m: number,
+  n: number
+): number[][] {
+  const result: number[][] = [];
+  if (m * n !== original.length) return result;
+  for (let i = 0; i < original.length; i++) {
+    if (i % n === 0) {
+      const row: number[] = [original[i]];
+      result.push(row);
+    } else {
+      result[result.length - 1].push(original[i]);
+    }
+  }
+  return result;
+}
+
+export {};
