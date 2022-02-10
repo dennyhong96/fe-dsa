b198278a2577425bb402cf37122f17b79c828b74
Denny Hong <hong961127@gmail.com>
2022-02-09 23:46:57 -0800

    Implement connect

---
+++ src/Pattern-BFS/116.connect.ts
@@ -0,0 +1,34 @@
+class Node {
+  val: number;
+  left: Node | null;
+  right: Node | null;
+  next: Node | null;
+
+  constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function connect(root: Node | null): Node | null {
+  if (!root) return null;
+  const queue: Node[] = [root];
+  while (queue.length) {
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift()!;
+      if (i < len - 1) {
+        node.next = queue[0];
+      } else {
+        node.next = null;
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return root;
+}
+
+export {};
