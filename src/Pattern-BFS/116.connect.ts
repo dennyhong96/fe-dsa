3cd8316dbd3067313e3208f536bb85bd449668ca
Denny Hong <hong961127@gmail.com>
2022-02-13 12:00:07 -0800

    Add 116.connect recursive solution

--- src/Pattern-BFS/116.connect.ts
+++ src/Pattern-BFS/116.connect.ts
@@ -12,6 +12,7 @@ class Node {
   }
 }
 
+// Iterative
 function connect(root: Node | null): Node | null {
   if (!root) return null;
   const queue: Node[] = [root];
@@ -29,6 +30,26 @@ function connect(root: Node | null): Node | null {
     }
   }
   return root;
+}
+
+// Recursive
+function connect2(
+  root: Node | null,
+  queue: (Node | null)[] = [root]
+): Node | null {
+  if (!root || queue[0] === null || !queue.length) return root;
+  const len = queue.length;
+  for (let i = 0; i < len; i++) {
+    const node = queue.shift()!;
+    if (i < len - 1) {
+      node.next = queue[0];
+    } else {
+      node.next = null;
+    }
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+  return connect2(root, queue);
 }
 
 export {};
