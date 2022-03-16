ed77b4b173918d58c301dfb965ae8b84e513f01c
Denny Hong <hong961127@gmail.com>
2022-03-15 22:07:39 -0700

    revisite sorting

--- src/13.Sorting/insertionSort.ts
+++ src/13.Sorting/insertionSort.ts
@@ -5,8 +5,8 @@ export function insertionSort(arr: number[]): void {
   for (let i = 1; i < arr.length; i++) {
     const unSortedItem = arr[i];
 
-    // Compare the current unsortd item with each of the previous sorted items
-    // to move the ucurrent unsortd item into it's proper index
+    // Compare the current unsorted item with each of the previous sorted items
+    // to move the current unsorted item into it's proper index
     for (let k = i - 1; k >= 0; k--) {
       const sortedItem = arr[k];
       if (unSortedItem < sortedItem) {
@@ -19,3 +19,8 @@ export function insertionSort(arr: number[]): void {
 // Use when input is small, or mostly sorted
 
 // [100, 70, 74, 30, 23, 200, 5, 95]
+
+// [3, 4, 5, 1, 7]
+//        k  i
+// [3, 1, 4, 5, 7]
+//     k     i
--- src/Pattern-DFS/100.isSameTree.ts
+++ src/Pattern-DFS/100.isSameTree.ts
@@ -3,7 +3,7 @@ import { TreeNode } from "../10.Trees/BinarySearchTree";
 // O(n) time; O(logn) space; (or O(h) space)
 export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
   if (!p && !q) return true;
-  if ((p && !q) || (!p && q) || (p && q && p.val !== q.val)) return false;
+  if ((p && !q) || (!p && q) || p!.val !== q!.val) return false;
   const isLeftValid = isSameTree(p!.left, q!.left);
   const isRightValid = isSameTree(p!.right, q!.right);
   return isLeftValid && isRightValid;
