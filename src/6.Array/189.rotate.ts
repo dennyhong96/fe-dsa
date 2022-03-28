1703d9135f664a3d940edf7b7656bb3dde7743f4
Denny Hong <hong961127@gmail.com>
2022-03-27 21:57:52 -0700

    Update rotate

--- src/6.Array/189.rotate.ts
+++ src/6.Array/189.rotate.ts
@@ -1,17 +1,16 @@
-// O(n+k) time; O(1) space;
+// O(n) time; O(1) space
 function rotate(nums: number[], k: number): void {
-  const len = nums.length;
-  k = k % nums.length; // handle k greater than nums array
+  const len = nums.length; // save original length
+  k = k % len; // handle k larger then nums length
+  // move all numbers to the right by k steps
   for (let i = nums.length - 1; i >= 0; i--) {
     const num = nums[i];
     nums[i + k] = num;
   }
+  // fill in first k numbers
   for (let i = 0; i < k; i++) {
-    const num = nums[i + len];
-    nums[i] = num;
+    nums[i] = nums[i + len];
   }
+  // shrink the array to be original length
   nums.length = len;
 }
-// [1,2,3,4,5,6,7]
-// [1,2,3,1,2,3,4,5,6,7]
-// [5,6,7,1,2,3,4]
