e7a29aece74bff665fc64846342a873b0510ecf4
Denny Hong <hong961127@gmail.com>
2022-04-20 11:56:40 -0700

    Update

--- src/practice/12.removePairs.ts
+++ src/practice/12.removePairs.ts
@@ -1,28 +1,30 @@
-function removePairs(str: string): number {
-  let maxRemovedCount = 0;
+// two pointer
 
-  const updateRemovedCount = (l: number, r: number) => {
-    let removePairsCount = 0;
+function removePairs(str: string): number {
+  let maxPairsRemoved = 0;
+
+  const remove = (l: number, r: number): void => {
+    let pairsRemoved = 0;
     while (l >= 0 && r < str.length) {
       if (
         (str[l] === "0" && str[r] === "1") ||
         (str[l] === "1" && str[r] === "0")
       ) {
-        removePairsCount++;
+        pairsRemoved++;
       } else {
         break;
       }
       l--;
       r++;
     }
-    maxRemovedCount = Math.max(maxRemovedCount, removePairsCount);
+    maxPairsRemoved = Math.max(maxPairsRemoved, pairsRemoved);
   };
 
   for (let i = 0; i < str.length - 1; i++) {
-    updateRemovedCount(i, i + 1);
+    remove(i, i + 1);
   }
 
-  return str.length - maxRemovedCount * 2;
+  return str.length - maxPairsRemoved * 2;
 }
 
 console.log(removePairs("01010"));
@@ -32,3 +34,5 @@ console.log(removePairs("111**000"));
 console.log(removePairs("01"));
 console.log(removePairs("00"));
 console.log(removePairs("010"));
+
+// probably can use one pass + a stack
